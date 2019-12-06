d757017c40e10920c2da2c9874ee9c1caad94476
malav <malavshah2018@gmail.com>
2019-12-06 15:44:18 -0500

    Created addLike api
    Yet to create routes

--- mixup/likes-comments.js
+++ mixup/likes-comments.js
@@ -15,25 +15,36 @@ module.exports = {
      * Create playlist
      * @param {*} req
      */
-    async addLikeComment(req) {
+    async addLike(req) {
         let playlistId = req.body.playlistId
         let userId = req.body.userId
-        let userName = req.body.userName
+        // let userName = req.body.userName
         let content = req.body.content
         let like = req.body.like
 
         utils.isString(playlistId, `playlistId ${playlistId}`)
         utils.isString(userId, `userId ${userId}`)
-        // utils.isString(content, `content ${content}`)
-        utils.isString(userName, `userName ${userName}`)
-        // utils.isString(like, `like ${like}`)
+        // utils.isString(userName, `userName ${userName}`)
+        utils.isString(content, `content ${content}`)
+        utils.isString(like, `like ${like}`)
 
+        // If playlistId alraedy exist Update add the userId who liked the playlist
+        let likesCollection = await likes()
+        let getPastLikesData = await likesCollection.findOne({ playlistId: playlistId })
+
+        if (getPastLikesData) {
+            getPastLikesData.userIds.push(userId)
+
+            let updateLikes = await likesCollection.updateOne({ _id: ObjectID(getPastLikesData._id) }, { $set: { getPastLikesData } })
+
+            return await this.getLikesById(getPastLikesData._id)
+        }
 
         // Create new like-comment object
         let newFeeds = {}
-        newFeeds.playlistI = playlistName
-        newFeeds.userId = userId
-        newFeeds.like = [userId]
+        newFeeds.playlistId = playlistId
+        // newFeeds.userId = userId
+        newFeeds.userIds = [userId]
         newFeeds.createdAt = new Date().toLocaleDateString()
 
         const likesCommentsCollection = await likesComments();
@@ -51,7 +62,7 @@ module.exports = {
      * Set playlist as deleted
      * @param {*} req 
      */
-    async removePlaylist(req) {
+    async removeLikes(req) {
         let playlistIds = req.body.playlistIds
         if (!playlistIds) throw "You must provide an id to search for";
 
@@ -79,61 +90,16 @@ module.exports = {
     },
 
     /**
-     * Update playlist info
-     * @param {*} req 
-     */
-    async updatePlaylist(req) {
-        let playlistId = req.body.playlistId
-        let playlistName = req.body.playlistName
-        // let songs = req.body.songs
-
-        if (!playlistId) throw "Invalid request";
-        if (!playlistName) throw "Invalid request";
-
-
-        const playlistCollection = await playlists();
-        const updatedPlaylist = {}
-        if (playlistName) {
-            updatedPlaylist.playlistName = playlistName
-        }
-
-        if (updatedPlaylist) {
-            updatedInfo.updatedAt = new Date().toLocaleDateString();
-            const updatedInfo = await playlistCollection.updateOne({ _id: ObjectID(playlistId) }, updatedPlaylist);
-
-            if (updatedInfo.modifiedCount === 0) {
-                throw "could not update playlist successfully";
-            }
-        }
-
-        return await this.getPlaylistById(id);
-    },
-
-    /**
      * Get a specific playlist
      * @param {*} id 
      */
-    async getLikesById(playlistId) {
-        if (!playlist) throw "You must provide an id to search for";
+    async getLikesById(id) {
+        if (!id) throw "You must provide an id to search for";
 
         const likesCommentsCollection = await likesComments();
-        const likesComments = await likesCommentsCollection.findOne({ playlistId: ObjectID(playlistId) });
-
-        if (likesComments === null) throw "No likesComments with that id";
-
-        !utils.isArray(likesComments.userIds, `userIds ${likesComments.userIds}`)
+        const likesComments = await likesCommentsCollection.findOne({ id: ObjectID(id) });
 
         return likesComments.userIds.length;
-    },
-
-    /**
-     * Get all playlists
-     */
-    async getAllPlaylists() {
-        const playlistCollection = await playlists();
-        const playlists = await playlistCollection.find({}).toArray();
-
-        return playlists;
     },
 
 };
