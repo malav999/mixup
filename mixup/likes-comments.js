669cf0708e4f0b320356e6f05873c888e3fa1b63
malav <malavshah2018@gmail.com>
2019-12-07 01:58:43 -0500

    Created addComment api

--- mixup/likes-comments.js
+++ mixup/likes-comments.js
@@ -19,17 +19,15 @@ module.exports = {
         let playlistId = req.body.playlistId
         let userId = req.body.userId
         // let userName = req.body.userName
-        let content = req.body.content
-        let like = req.body.like
+        // let content = req.body.content
 
         utils.isString(playlistId, `playlistId ${playlistId}`)
         utils.isString(userId, `userId ${userId}`)
         // utils.isString(userName, `userName ${userName}`)
-        utils.isString(content, `content ${content}`)
-        utils.isString(like, `like ${like}`)
+        // utils.isString(content, `content ${content}`)
 
         // If playlistId alraedy exist Update add the userId who liked the playlist
-        let likesCollection = await likes()
+        let likesCollection = await likesComments()
         let getPastLikesData = await likesCollection.findOne({ playlistId: playlistId })
 
         if (getPastLikesData) {
@@ -37,7 +35,7 @@ module.exports = {
 
             let updateLikes = await likesCollection.updateOne({ _id: ObjectID(getPastLikesData._id) }, { $set: { getPastLikesData } })
 
-            return await this.getLikesById(getPastLikesData._id)
+            return await this.getLikesCommentsById(getPastLikesData._id)
         }
 
         // Create new like-comment object
@@ -55,7 +53,7 @@ module.exports = {
         if (insertInfo.insertedCount === 0) throw "Could not add like-comment";
         const newId = insertInfo.insertedId;
 
-        return this.getLikesById(newId)
+        return this.getLikesCommentsById(newId)
     },
 
     /**
@@ -93,7 +91,7 @@ module.exports = {
      * Get a specific playlist
      * @param {*} id 
      */
-    async getLikesById(id) {
+    async getLikesCommentsById(id) {
         if (!id) throw "You must provide an id to search for";
 
         const likesCommentsCollection = await likesComments();
@@ -102,4 +100,60 @@ module.exports = {
         return likesComments.userIds.length;
     },
 
+    /**
+     * Add comment to a playlist
+     * @param {*} req
+     */
+    async addComment(req) {
+        let playlistId = req.body.playlistId
+        let userId = req.body.userId
+        // let userName = req.body.userName
+        let content = req.body.content
+        // let like = req.body.like
+
+        utils.isString(playlistId, `playlistId ${playlistId}`)
+        utils.isString(userId, `userId ${userId}`)
+        utils.isString(content, `content ${content}`)
+        // utils.isString(like, `like ${like}`)
+        // utils.isString(userName, `userName ${userName}`)
+
+
+        // If playlistId alraedy exist Update add the userId who liked the playlist
+        let commentCollection = await likesComments()
+        let getPastCommentData = await commentCollection.findOne({ playlistId: playlistId })
+
+        if (getPastCommentData) {
+            let contentObj = {}
+            contentObj.userId = userId
+            contentObj.name = userName
+            contentObj.comment = comment
+            getPastCommentData.content.push(contentObj)
+
+            let updateComment = await commentCollection.updateOne({ _id: ObjectID(getPastCommentData._id) }, { $set: { getPastCommentData } })
+
+            return await this.getLikesCommentsById(getPastCommentData._id)
+        }
+
+        let contentObj = {}
+        contentObj.userId = userId
+        contentObj.name = userName
+        contentObj.comment = comment
+
+        // Create new like-comment object
+        let newFeeds = {}
+        newFeeds.playlistId = playlistId
+        // newFeeds.userId = userId
+        newFeeds.content = [contentObj]
+        newFeeds.createdAt = new Date().toLocaleDateString()
+
+        const likesCommentsCollection = await likesComments();
+
+        // Add likes-comments
+        const insertInfo = await likesCommentsCollection.insertOne(newFeeds);
+        // If insertion fails, err
+        if (insertInfo.insertedCount === 0) throw "Could not add like-comment";
+        const newId = insertInfo.insertedId;
+
+        return this.getLikesCommentsById(newId)
+    }
 };
