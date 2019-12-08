ce98760e8e781922d0e38870d84468581631187c
malav <malavshah2018@gmail.com>
2019-12-07 19:12:45 -0500

    Created likesComments routes

--- mixup/index.js
+++ mixup/index.js
@@ -1,9 +1,11 @@
 const userData = require("./user");
 const playlistData = require("./playlist");
 const songData = require("./song");
+const likesCommentsData = require("./likesComments");
 
 module.exports = {
   user: userData,
   playlist: playlistData,
   song: songData,
+  likesComments: likesCommentsData
 };
